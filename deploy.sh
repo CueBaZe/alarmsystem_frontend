0a16e24b159d597a1a91f8efd6f5e0c0481b8970
CueBaZe <olivervalente@icloud.com>
2026-09-03 12:39:16 +0200

    fix: streamline deploy script and optimize imports in login component

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 #Run this to build and deploy the website to production
-set -e
-
 echo "Building production bundle..."
-npm run build
+npm.cmd run build
 
 echo "Syncing files to server..."
 scp -r dist/* gr3@192.168.111.20:/var/www/alarmsystem_frontend/
--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,6 +1,6 @@
 import logo from '../assets/firewall.png'
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
